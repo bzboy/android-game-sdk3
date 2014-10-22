73bd03ba7e1a1a7481a08ad1af70b0012a0e872f
Salem Duy <duynk@appota.com>
2014-10-22 17:51:23 +0700

    remove FragmentActivity

--- samples/src/com/appota/sample/gamesdk/MainActivity.java
+++ samples/src/com/appota/sample/gamesdk/MainActivity.java
@@ -4,24 +4,28 @@ package com.appota.sample.gamesdk;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
+import com.appota.facebook.Session;
 import com.appota.gamesdk.commons.AppotaAction;
 import com.appota.gamesdk.core.AppotaGameSDK;
 import com.appota.gamesdk.core.AppotaReceiver;
 import com.appota.gamesdk.model.AppotaSession;
 import com.appota.gamesdk.model.TransactionResult;
-import com.facebook.Session;
+import com.appota.sample.gamesdk1.R;
+import com.appota.support.v4.app.AppotaActivity;
+import com.onclan.android.OnClanSDK;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppotaActivity {
 
-    private String apiKey = "123593a5f93eac19e26baee408f9928f0525e6a18";
+    //private String apiKey = "123593a5f93eac19e26baee408f9928f0525e6a18";
+    private String apiKey = "0e08c327e1ba11e9ac5d23fc86b4c96e053bf65b8";
     private String sandboxApiKey = "";
     private AppotaGameSDK sdk;
+    private OnClanSDK onClanSDK;
     private LoginReceiver receiver;
 
     @Override
@@ -36,17 +40,19 @@ public class MainActivity extends FragmentActivity {
         filter.addAction(AppotaAction.PAYMENT_SUCCESS_ACTION);
         filter.addAction(AppotaAction.SWITCH_SUCCESS_ACTION);
         filter.addAction(AppotaAction.LOGIN_FAIL_ACTION);
+        filter.addAction(AppotaAction.LOGOUT_SUCCESS_ACTION);
         registerReceiver(receiver, filter);
         //init sdk
-        AppotaGameSDK.getInstance().setAutoLogin(false);
+        AppotaGameSDK.getInstance().setAutoLogin(true);
+        onClanSDK = OnClanSDK.getInstance();
         sdk = AppotaGameSDK.getInstance();
-        sdk.init(this, "https://developer.appota.com/config.php", true, "http://filestore9.com/test.php", apiKey, sandboxApiKey);
+        sdk.init(this, "https://developer.appota.com/config.php", false, "http://filestore9.com/test.php", apiKey, sandboxApiKey);
         //optional
-        sdk.setShowButtonType(AppotaGameSDK.SHOW_ACCOUNT_BUTTON);
+        //sdk.setShowButtonType(AppotaGameSDK.SHOW_ACCOUNT_BUTTON);
 
         //show or hide switch, logout button
-        sdk.setShowUserFunctionButtons(false);
-        sdk.prepareFacebookLogin(this, savedInstanceState);
+//        sdk.setShowUserFunctionButtons(false);
+//        sdk.prepareFacebookLogin(this, savedInstanceState);
         //we use 2 way to login facebook: native and web
         //sdk.setLoginFacebookType(AppotaGameSDK.LOGIN_FACEBOOK_NATIVE);
         //sdk.setLoginFacebookType(AppotaGameSDK.LOGIN_FACEBOOK_WEB);
@@ -63,8 +69,8 @@ public class MainActivity extends FragmentActivity {
     }
     
     public void manualLogin(View v){
-    	//sdk.manualLogin();
-    	sdk.showLogin();
+    	sdk.manualLogin();
+    	//sdk.showLogin();
     	//sdk.isUserLogin();
 //    	List<PaymentMethod> list = sdk.getListPaymentMethod();
 //    	for(PaymentMethod method : list){
@@ -127,11 +133,12 @@ public class MainActivity extends FragmentActivity {
         public void onLoginSuccess(AppotaSession user) {
             //do verify login with your server now
             Toast.makeText(MainActivity.this, "Just for login testing. Username = " + user.username + ", AccessToken= " + user.accessToken, Toast.LENGTH_SHORT).show();
+            onClanSDK.initOnClanSDK(MainActivity.this, user.getOnClanUser());
         }
 
         @Override
         public void onLogoutSuccess() {
-
+        	onClanSDK.logoutOnClan();
         }
 
         @Override
@@ -155,6 +162,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onDestroy() {
         sdk.finish();
+        onClanSDK.destroy();
         unregisterReceiver(receiver);
         super.onDestroy();
     }
